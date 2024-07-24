e3b4659742eed757c47bc9b258c6dfed34c36aad
Sapna <Sapna.suvarna@gupshup.io>
2024-07-25 00:46:54 +0530

    second commit

--- src/Homework/FizzBuzz.java
+++ src/Homework/FizzBuzz.java
@@ -7,17 +7,19 @@ public class FizzBuzz {
 
         for(int i=1; i<=100;i++)
         {
-            if (i%3==0)
+            if ((i%3==0) && (i%5!=0))
             {
                 System.out.println("Fizz");
             }
-            if (i%5==0) {
+            if ((i%5==0) && (i%3!=0))
+            {
                 System.out.println("Buzz");
             }
             if ((i%3==0) && (i%5==0)) {
                 System.out.println("FizzBuzz");
-
-
+            }
+           else if ((i%3!=0) && (i%5!=0)) {
+                System.out.println(i);
             }
 
         }
