b2f53684b8e32dc4324d655c3a13ea794011aae5
Sapna <Sapna.suvarna@gupshup.io>
2024-07-25 00:29:29 +0530

    second commit

---
+++ src/Homework/FizzBuzz.java
@@ -0,0 +1,25 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=100;i++)
+        {
+            if (i%3==0)
+            {
+                System.out.println("Fizz");
+            }
+            if (i%5==0) {
+                System.out.println("Buzz");
+            }
+            if ((i%3==0) && (i%5==0)) {
+                System.out.println("FizzBuzz");
+
+
+            }
+
+        }
+    }
+}
