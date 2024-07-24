941ef1e77ee1d43a570efbd4e69fb8728495ea7a
Sapna <Sapna.suvarna@gupshup.io>
2024-07-25 00:13:09 +0530

    second commit

--- src/Homework/TriangleClassifier.java
+++ src/Homework/TriangleClassifier.java
@@ -22,6 +22,8 @@ public class TriangleClassifier {
         else if((side1==side2 && side2!=side3) || (side2==side3 && side2!=side1) || (side3==side1 && side3!=side2))
         {
             System.out.println("It is isoceles traingle");
+
         }
+
     }
 }
